72599c6fb35b21bc3d5dee6be531ef025b98752f
Bela <belahamid@gmail.com>
2021-02-16 14:26:35 +0000

    created VehicleType enum

--- src/main/java/car/VehicleType.java
+++ src/main/java/car/VehicleType.java
@@ -1,4 +1,27 @@
 package car;
 
 public enum VehicleType {
+
+    ELECTRIC (50000.00, "red"),
+    HYBRID (45000.00, "metallic"),
+    PETROL (35000.00, "blue"),
+    DIESEL (30000.00, "racing green");
+
+    private final double price;
+    private final String colour;
+
+    VehicleType(double price, String colour){
+        this.price = price;
+        this.colour = colour;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public String getColour() {
+        return colour;
+    }
 }
