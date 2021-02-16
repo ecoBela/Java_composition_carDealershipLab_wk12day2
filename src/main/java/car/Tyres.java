2d902c2aaf64c69c0fb225b2188c4ff12d8b317a
Bela <belahamid@gmail.com>
2021-02-16 15:03:37 +0000

    test passed: tyres have treadwear and type

--- src/main/java/car/Tyres.java
+++ src/main/java/car/Tyres.java
@@ -1,4 +1,19 @@
 package car;
 
 public class Tyres {
+    private double treadWear;
+    private String tyreType;
+
+    public Tyres(double treadWear, String tyreType){
+        this.treadWear = treadWear;
+        this.tyreType = tyreType;
+    }
+
+    public double getTreadWear() {
+        return treadWear;
+    }
+
+    public String getTyreType() {
+        return tyreType;
+    }
 }
---
+++ src/test/java/TyresTest.java
@@ -0,0 +1,25 @@
+import car.Tyres;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TyresTest {
+    Tyres tyres;
+
+    @Before
+    public void before(){
+        tyres = new Tyres(1.6, "summer");
+    }
+
+    @Test
+    public void hasTreadWear(){
+        assertEquals(1.6, tyres.getTreadWear(), 0.01);
+    }
+    @Test
+    public void hasType(){
+        assertEquals("summer", tyres.getTyreType());
+    }
+
+
+}
